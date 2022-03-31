d31dded55ebf380a1d29772920af4cd47da20b4a
Jim Grady <jimgrady.jg@gmail.com>
2022-03-31 09:24:55 -0400

    Merge from origin.

--- py-doc-to-xml/main.py
+++ py-doc-to-xml/main.py
@@ -3,10 +3,12 @@
 from __future__ import annotations
 
 from pathlib import Path
+from typing import Optional
 
 from docx import Document
 from docx_to_xml.doc1 import process_doc
 from docx_to_xml.types import DocModel
+from pprint import pprint
 from simplify_docx import simplify
 from typer import Option, run
 
@@ -28,9 +30,13 @@ def display_model(model: DocModel, *, depth: int = 0) -> None:
 def main(
     doc_file: Path = Option(..., "--doc-file", "-d", exists=True, dir_okay=False, readable=True, resolve_path=True),
     output_file: Path = Option(..., "--output-file", "-o", writable=True, resolve_path=True),
+    debug_file: Optional[Path] = Option(writable=True, resolve_path=True, default=None),
 ) -> None:
     doc = Document(doc_file)
     json = simplify(doc)
+    if debug_file is not None:
+        with open(debug_file, "w") as json_file:
+            pprint(json, stream=json_file)
     doc_model: DocModel = DocModel.parse_obj(json)
     display_model(doc_model)
 
